951848a2a25e53bfdfae5f33414b5856211fb813
NelsonGomesNeto <ngn@ic.ufal.br>
2018-08-24 12:50:12 -0300

    Interesting Injection :)

--- Exercise 4/src/main/java/Reader.java
+++ Exercise 4/src/main/java/Reader.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Reader {
@@ -9,22 +10,66 @@ public class Reader {
         this.fileInputStream = fileInputStream;
     }
 
-    public static String readAll(String path) {
-        try {
-            File file = new File(path);
-            FileInputStream fileInputStream = new FileInputStream(file);
-            StringBuilder string = new StringBuilder();
-            int aux;
-            try {
-                while ((aux = fileInputStream.read()) != -1)
-                    string.append((char) aux);
-                fileInputStream.close();
-                return(string.toString());
-            } catch (IOException exception) {
-                return(null);
+    public static String readAll(FileInputStream in) throws IOException {
+
+        ArrayList<Byte> bytes = new ArrayList<>();
+
+        int size;
+        while ((size = in.available()) != 0) {
+            byte[] buff = new byte[size];
+
+            while (size > 0) {
+                int read = in.read(buff);
+                for (byte b: buff)
+                    bytes.add(b);
+                buff = new byte[size - read];
+                size -= read;
             }
-        } catch (FileNotFoundException exception) {
-            return(null);
+        }
+
+        byte[] buff = new byte[bytes.size()];
+        for (int i = 0; i < bytes.size(); i ++) {
+            buff[i] = bytes.get(i);
+        }
+
+        return(new String(buff));
+    }
+
+    public static String prevReadAll(FileInputStream in) throws IOException {
+        byte[] buff = new byte[in.available()];
+
+        in.read(buff);
+
+        return(new String(buff));
+    }
+
+    public static class MockFileInputStream extends FileInputStream {
+
+        long size;
+
+        public MockFileInputStream(File file) throws FileNotFoundException {
+            super(file);
+            size  = file.length();
+        }
+
+        @Override
+        public int read(byte[] buff) throws IOException {
+            int size = buff.length < 2 ? buff.length : buff.length - 1;
+
+            byte[] myBytes = new byte[size];
+            int read = super.read(myBytes);
+            for (int i = 0; i < myBytes.length; i ++) {
+                buff[i] = myBytes[i];
+            }
+            return(read);
+        }
+
+        @Override
+        public int available() {
+            if (size == 0) return(0);
+            int result = (int) Math.min(3, size);
+            size -= result;
+            return(result);
         }
     }
 }
--- Exercise 4/src/test/java/ReaderTest.java
+++ Exercise 4/src/test/java/ReaderTest.java
@@ -1,21 +1,26 @@
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ReaderTest {
 
     @Test
-    void readAll() {
+    void readAll() throws IOException {
         String expectedASCII = "LoL" + System.lineSeparator() + System.lineSeparator() + "Miau";
         String expectedUnicode = "\uD83C\uDD5B\uD83C\uDD5E\uD83C\uDD5B";
         String pathASCII = "src/test/resources/test";
+        FileInputStream ASCIIstream = new Reader.MockFileInputStream(new File(pathASCII));
         String pathUnicode = "src/test/resources/unicode";
+        FileInputStream UnicodeStream = new Reader.MockFileInputStream(new File(pathUnicode));
         assertAll(
-                () -> assertNull(Reader.readAll("not existent"), "String not null for not existent file"),
-                () -> assertEquals(expectedASCII, Reader.readAll(pathASCII), "ASCII strings are different"),
-                () -> assertEquals(expectedUnicode, Reader.readAll(pathUnicode), "Unicode strings are different")
+//                () -> assertNull(Reader.readAll(null), "String not null for not existent file"),
+                () -> assertEquals(expectedASCII, Reader.readAll(ASCIIstream), "ASCII strings are different"),
+                () -> assertEquals(expectedUnicode, Reader.readAll(UnicodeStream), "Unicode strings are different")
         );
     }
 }
